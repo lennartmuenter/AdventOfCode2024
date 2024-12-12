2b100e81c90987d8d51142e56a2048ec4ba08382
mauveyone <lennart@muenter.eu>
2024-12-12 09:02:02 +0100

    broke into smaller problems

--- csharp/Day11.cs
+++ csharp/Day11.cs
@@ -2,10 +2,19 @@ namespace AdventOfCode2024;
 
 public class Day11
 {
+    private static Dictionary<long, (long, long)> _cache = new Dictionary<long, (long, long)>();
     public static void Run()
     {
         var stones = File.ReadAllText("../../../../csharp/day11.txt").Trim().Split(" ").Select(long.Parse).ToList();
-        for (var count = 0; count < 25; count++)
+        var count = stones.Select(stone => check(stone, 75)).Sum();
+        Console.WriteLine(count);
+    }
+
+    private static long check(long start, int depth)
+    {
+        var stones = new List<long>();
+        stones.Add(start);
+        for (var count = 0; count < depth; count++)
         {
             Console.WriteLine(count);
             for (var i = 0; i < stones.Count; i++)
@@ -16,10 +25,18 @@ public class Day11
                     stones[i] = 1;
                 } else if (length % 2 == 0)
                 {
+                    if (_cache.ContainsKey(stones[i]))
+                    {
+                        var key = stones[i];
+                        stones[i] = _cache[key].Item1;
+                        stones.Insert(++i, _cache[key].Item2);
+                        continue;
+                    }
                     var num = stones[i];
                     var split = (long)Math.Pow(10, length / 2);
                     stones[i] = num / split;
                     stones.Insert(i+1, num % split);
+                    _cache.Add(num, (stones[i], stones[i+1]));
                     i++;
                 }
                 else
@@ -28,6 +45,7 @@ public class Day11
                 }
             }
         }
-        Console.WriteLine(stones.Count);
+
+        return stones.Count;
     }
 }
