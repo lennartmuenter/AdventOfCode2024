f7fba1a0bc36c74d92ee37df1ba8f5dfb9ab1519
mauveyone <lennart@muenter.eu>
2024-12-12 12:55:18 +0100

    day 11

--- csharp/Day11.cs
+++ csharp/Day11.cs
@@ -1,74 +1,43 @@
-using System.Collections.Concurrent;
-using System.Data;
-
 namespace AdventOfCode2024;
 
 public class Day11
 {
-    private static readonly ConcurrentDictionary<long, (long, long)> _cache = new();
-    private static long _count = 0;
     public static void Run()
     {
-        var stones = File.ReadAllText("../../../../csharp/day11.txt").Trim().Split(" ").Select(long.Parse).ToList();
-        const int depth = 40;
-        const int chunk = 5;
-        Thread lastThr = null;
-        var watch = System.Diagnostics.Stopwatch.StartNew();
-        stones.ForEach(stone =>
+        var dict = File.ReadAllText("../../../../csharp/day11.txt").Trim().Split(" ").Select(long.Parse).ToList().ToDictionary(stone => stone, _ => 1L);
+        for (var count = 0; count < 75; count++)
         {
-            var t = new Thread(() => check(stone, chunk, depth - chunk));
-            t.Start();
-            t.Join();
-            lastThr = t;
-        });
-        watch.Stop();
-        Console.WriteLine(_count);
-        Console.WriteLine(watch.ElapsedMilliseconds);
-    }
-
-    private static void check(long start, int depth, int remainingDepth)
-    {
-        var stones = new List<long> { start };
-        for (var currentDepth = 0; currentDepth < depth; currentDepth++)
-        {
-            for (var i = 0; i < stones.Count; i++)
+            var newDict = dict.ToDictionary();
+            foreach (var stone in dict)
             {
-                var length = Math.Floor(Math.Log10(stones[i]) + 1);
-                if (stones[i] == 0)
+                if (stone.Value <= 0) continue;
+                var length = Math.Floor(Math.Log10(stone.Key) + 1);
+                if (stone.Key == 0)
                 {
-                    stones[i] = 1;
+                    if (newDict.ContainsKey(1)) newDict[1] += stone.Value;
+                    newDict.TryAdd(1, stone.Value);
+                    newDict[0] -= stone.Value;
                 } else if (length % 2 == 0)
                 {
-                    if (_cache.ContainsKey(stones[i]))
-                    {
-                        var key = stones[i];
-                        stones[i] = _cache[key].Item1;
-                        stones.Insert(++i, _cache[key].Item2);
-                        continue;
-                    }
-                    var num = stones[i];
-                    var split = (long)Math.Pow(10, length / 2);
-                    stones[i] = num / split;
-                    stones.Insert(i+1, num % split);
-                    _cache.TryAdd(num, (stones[i], stones[i + 1]));
-                    i++;
+                    var split = (long) Math.Pow(10, length / 2);
+                    if (newDict.ContainsKey(stone.Key / split)) newDict[stone.Key / split] += stone.Value;
+                    newDict.TryAdd(stone.Key / split, stone.Value);
+
+                    if (newDict.ContainsKey(stone.Key % split)) newDict[stone.Key % split] += stone.Value;
+                    newDict.TryAdd(stone.Key % split, stone.Value);
+                    newDict[stone.Key] -= stone.Value;
+                    
                 }
                 else
                 {
-                    stones[i] *= 2024;
+                    if (newDict.ContainsKey(stone.Key * 2024)) newDict[stone.Key * 2024] += stone.Value;
+                    newDict.TryAdd(stone.Key * 2024, stone.Value);
+                    newDict[stone.Key] -= stone.Value;
                 }
             }
+            dict = newDict;
         }
-        if (remainingDepth == 0)
-        {
-            _count += stones.Count;
-        }
-        else
-        {
-            stones.ForEach(stone =>
-            {
-                check(stone, depth, remainingDepth - depth);
-            });
-        }
+
+        Console.WriteLine(dict.Sum(keyValuePair => keyValuePair.Value));
     }
 }
